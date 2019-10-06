a0414ebb1f32438561468abc16d976fbaf580ff8
Arish-Shah <arish0246@gmail.com>
2019-10-06 14:54:12 +0530

    Added useEffect for users

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 5000;
 
 app.get('/api/users', (req, res) => {
 	const users = [
 		{ id: 0, firstName: 'John', lastName: 'Doe' },
 		{ id: 1, firstName: 'Mary', lastName: 'Swason' },
-		{ id: 2, firstName: 'Arish', lastName: 'Shah' }
+		{ id: 2, firstName: 'Steve', lastName: 'Smith' }
 	];
 
 	res.json(users);
 });
 
-app.listen(port, () => console.log(`Server started on port ${port}.`));
+const port = 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
