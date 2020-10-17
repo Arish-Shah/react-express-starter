bcf99621d39eb980cfc61f6aadb2970bed5d2d95
Arish Shah <arish0246@gmail.com>
2020-10-17 22:58:33 +0530

    update packages

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/api/users', (req, res) => {
   const users = [
     { id: 0, firstName: 'John', lastName: 'Doe' },
     { id: 1, firstName: 'Mary', lastName: 'Swason' },
-    { id: 2, firstName: 'Steve', lastName: 'Smith' },
-  ]
+    { id: 2, firstName: 'Steve', lastName: 'Smith' }
+  ];
 
-  res.json(users)
-})
+  res.status(200).json(users);
+});
 
-const port = 5000
+const port = 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`));
